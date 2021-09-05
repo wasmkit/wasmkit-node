718fd917f225d4b22191714a98648c25a4d74e67
Andrew <68395886+awt-256@users.noreply.github.com>
2021-09-05 19:01:21 -0400

    add example usage

---
+++ examples/simple.js
@@ -0,0 +1,26 @@
+const util = require('util');
+const WabtModule = require('./wabt');
+const { WasmParser } = require("../dist/parser");
+
+WabtModule().then(function(wabt) {
+    const text = `(module
+        (import "console" "log" (func $console.log (param i32)))
+  
+        (func (export "logOne")
+          i32.const 0
+          call $console.log))`;
+    const file = "simple.wat";
+
+    const wat = wabt.parseWat(file, text)
+    const {buffer} = wat.toBinary({});
+
+    try {
+        const wasmModule = WasmParser.decodeFrom(buffer);
+
+        console.log(file + "\n\n" + text + "\n\n");
+        console.log(util.inspect(wasmModule, false, 27, true));
+    } catch (err) {
+        console.log(err);
+        console.log(buffer);
+    }
+});
